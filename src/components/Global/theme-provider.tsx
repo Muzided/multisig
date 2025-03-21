debfc1051dd9176537b458cf598bbdb2e28f4982
muzided <muzammillone07@gmail.com>
2025-03-21 20:30:19 +0500

    theme changes

--- src/components/Global/theme-provider.tsx
+++ src/components/Global/theme-provider.tsx
@@ -1,9 +1,9 @@
-"use client";
-
-import { ThemeProvider as NextThemesProvider } from "next-themes";
-
-type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
+"use client"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+import type { ThemeProviderProps } from "next-themes"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  // No direct document/window access here
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+

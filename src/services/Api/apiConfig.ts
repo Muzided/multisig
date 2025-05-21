2618c512ef3caa7dec1d16f7e172aa7b359b6f40
muzided <muzammillone07@gmail.com>
2025-05-21 15:26:38 +0500

    changed backend url

--- src/services/Api/apiConfig.ts
+++ src/services/Api/apiConfig.ts
@@ -29,7 +29,7 @@ class AxiosService {
 
   private constructor() {
     this.axiosInstance = axios.create({
-      baseURL: 'http://localhost:5000/',
+      baseURL: 'https://multisig-escrow-backend.onrender.com/',
       timeout: 50000,
       headers: {
         'Content-Type': 'application/json',

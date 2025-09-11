aacca44271ef57102005b1331227b79606f14562
muzided <muzammillone07@gmail.com>
2025-09-11 15:34:56 +0500

    added new factory address

--- src/Web3/web3-config.ts
+++ src/Web3/web3-config.ts
@@ -1,5 +1,5 @@
 
-export const MultiSig_Factory_Address ="0xd0cd6cf3dec1fdec1c37645a400d2e99956789a1";
+export const MultiSig_Factory_Address ="0x3ec20e0d4d5e454df14f0075b18fa90059510ea4";
 //0xA4b46929966c929fafd67986Dfb0507F9Ffc14f1 verifed contract address
 export const Usdt_Contract_Address ="0xc365Fc4c14259b861Dddbe771618ab9258da32f3"
 export const escrow_Contract_Address ="0x949e1BA0ff30a792D1C4e7276dDCAC923187BAB5";
@@ -7,3 +7,6 @@ export const tokenDecimals = 6;
 export const kyc_status = false;
 export const infuraRpcUrl = "https://sepolia.infura.io/v3/38908a5032784374a5536d93c06aac59"
 
+
+
+// new usdt contract address --- 0x3ec20e0d4d5e454df14f0075b18fa90059510ea4
